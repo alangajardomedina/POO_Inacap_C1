0ee3fbcc1c9a84c1849cfee6a56a57841e81f62a
alangajardomedina <alan.gajardo03@inacapmail.cl>
2025-10-24 15:36:26 -0300

    actualización - se agrega ejemplo de herencia

---
+++ clase3_24_10_2025/ejemplo_herencia/auto.py
@@ -0,0 +1,18 @@
+from vehiculo import Vehiculo
+
+class Auto(Vehiculo):
+    __cant_puertas = 3
+
+    def __init__(self, patente, marca, modelo, cant_puertas):
+        super().__init__(patente, marca, modelo)
+        self.__cant_puertas = cant_puertas
+    
+    def getCantidadPuertas(self):
+        return self.__cant_puertas
+    
+    def setCantidadPuertas(self,cant_puertas):
+        self.__cant_puertas = cant_puertas
+    
+    def __str__(self):
+        return super().__str__() + f" - Cant. puertas: {self.__cant_puertas}"
+    
---
+++ clase3_24_10_2025/ejemplo_herencia/main.py
@@ -0,0 +1,14 @@
+from moto import Moto
+from auto import Auto
+
+moto1 = Moto("WKS-21","Honda","XR",True)
+moto2 = Moto("THR-42","BMW","S1000rr",False)
+
+auto1 = Auto("GGWP-15","Chevrolet","Camaro",3)
+auto2 = Auto("GLHF-52","Toyota","Supra",3)
+
+print(moto1)
+print(moto2)
+
+print(auto1)
+print(auto2)
